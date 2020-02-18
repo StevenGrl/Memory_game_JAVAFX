5279261624b6b9f70bd0f41f843faac476308805
sgrill02 <sgrill02@BE19-15-L.sfasp2mi.univ-poitiers.fr>
2020-02-18 10:15:36 +0100

    starting grid  working git add .git add .git add .

--- src/memory/Main.java
+++ src/memory/Main.java
@@ -2,33 +2,66 @@ package memory;
 
 import javafx.application.Application;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.shape.Circle;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import training.exercice3.Dessin;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main extends Application {
+    private static final int NUMBER_OF_PAIRS = 8;
+    private static final int NUMBER_PER_ROW = 4;
 
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World");
-        StackPane root = new StackPane();
-        HBox topBox = new HBox();
-        topBox.getChildren().add(new Dessin(new Circle()));
-        topBox.setAlignment(Pos.TOP_CENTER);
-        root.getChildren().add(topBox);
+    private Parent createContent() {
+        Pane root = new Pane();
+        root.setPrefSize(600,600);
+        char c = 'A';
+        List<Tile> tiles = new ArrayList<>();
+        for (int i = 0; i < NUMBER_OF_PAIRS; i++) {
+            tiles.add(new Tile(String.valueOf(c)));
+            tiles.add(new Tile(String.valueOf(c)));
+            c++;
+        }
+        Collections.shuffle(tiles);
 
-        HBox botBox = new HBox();
-        botBox.getChildren().add(new Dessin(new Rectangle()));
-        botBox.setAlignment(Pos.BOTTOM_CENTER);
-        root.getChildren().add(botBox);
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        for(int i = 0; i < tiles.size(); i++) {
+            Tile tile = tiles.get(i);
+            tile.setTranslateX(50 * (i % NUMBER_PER_ROW));
+            tile.setTranslateY(60 * (i / NUMBER_PER_ROW));
+            root.getChildren().add(tile);
+        }
+
+        return root;
     }
 
+    private static class Tile extends StackPane {
+        Tile(String value) {
+            Rectangle border = new Rectangle(50, 60);
+            border.setFill(null);
+            border.setStroke(Color.BLACK);
+
+            Text text = new Text();
+            text.setText(value);
+            text.setFont(Font.font(30));
+
+            setAlignment(Pos.CENTER);
+            getChildren().addAll(border, text);
+        }
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(createContent()));
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
         launch(args);
--- src/memory/models/Card.java
+++
@@ -1,4 +0,0 @@
-package memory.models;
-
-public class Card {
-}
